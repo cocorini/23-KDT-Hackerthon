b863ed1900cd2cdb35ce242e811aba9bb9c5b3ff
sungup <wsup97@gmail.com>
2023-10-16 17:52:21 +0900

    사람 머리, 얼굴만 추출해서 파일에 저장하는 코드

--- import_box.py
+++ import_box.py
@@ -1,32 +1,38 @@
 import cv2
 import json
+import numpy as np
+import os
 
-# 이미지 불러오기
-image = cv2.imread('C:\Users\user\Desktop\23_kdt_hackathon\23-KDT-Hackerthon\hackathon_data\data\Training\01.원천데이터\TS_남자사람\남자사람_7_남_00572.jpg')
+img_array = np.fromfile(r"C:\Users\user\Desktop\23_kdt_hackathon\hackathon_data\data\Training\01.원천데이터\TS_남자사람\남자사람_7_남_00572.jpg", np.uint8)
+image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+file_path = r"C:\Users\user\Desktop\23_kdt_hackathon\hackathon_data\data\Training\02.라벨링데이터\TL_남자사람\남자사람_7_남_00572.json"
 
 with open(file_path, "r", encoding="utf-8") as file:
     json_data = json.load(file)
     
-    # 사람 전체~입까지 바운딩 박스 정보 탐색
-    for cnt in range(0, 7):
+    for cnt in range(1, 3):
         json_data_ele = json_data['annotations']['bbox'][cnt]
-        width, height = json_data_ele['w'], json_data_ele['h']  #좌표 및 크기
-        size = width * height
+        x, y, width, height = json_data_ele['x'], json_data_ele['y'], json_data_ele['w'], json_data_ele['h']  #좌표
 
+        # Bounding box에 해당하는 이미지 추출
+        roi = image[y:y+height, x:x+width]
 
-# Bounding box 좌표 (x, y, width, height)
-x, y, width, height = (100, 100, 200, 200)  # 원하는 좌표 및 크기로 수정하세요
+        # 추출한 이미지를 저장할 경로 지정
+        output_path = r"C:\Users\user\Desktop\23_kdt_hackathon\hackathon_data\data\Training\01.원천데이터\TS_남자사람_"
 
-# Bounding box에 해당하는 이미지 추출
-roi = image[y:y+height, x:x+width]
+        output_path += json_data['annotations']['bbox'][cnt]['label']
+        
+        file_name = json_data['meta']['img_path'].split('/')[-1]
+        
+        output_path = os.path.join(output_path, file_name)
+         
+        ret, img_arr = cv2.imencode('.jpg', roi)
 
-# 추출한 이미지를 저장할 경로 지정
-output_path = 'output_image.jpg'
+        if ret:
+            with open(output_path, mode='w+b') as f:
+                img_arr.tofile(f)
 
-# 이미지 저장
-cv2.imwrite(output_path, roi)
-
-# 추출한 이미지를 화면에 표시 (선택사항)
-cv2.imshow('Extracted Image', roi)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        # 추출한 이미지를 화면에 표시 (선택사항)
+        cv2.imshow('Extracted Image', roi)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
